e50c12c0c80b6d587c7658536d84e6b7082b8ea0
NAD00400 <anhduynguyenkhuat@gmail.com>
2024-09-27 00:05:29 +0700

    video43

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import './Components/todo/todo.css'
 import { TodoData } from './Components/todo/TodoData'
 import { TodoNew } from './Components/todo/TodoNew'
 const App =()=> {
+
+  const name="Eric Arsenal";
+  const age=25;
+  const data={
+    address: 'ha noi',
+    country:'viet nam'
+    };
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew/>
-      <TodoData/>
+      <TodoData
+        name={name}
+        data={data}
+        age={age}
+      />
       <div className='todo-image'>
         <img src={reactLogo} alt="React Logo" className='logo'/>
       </div>
--- src/Components/todo/TodoData.jsx
+++ src/Components/todo/TodoData.jsx
@@ -1,9 +1,20 @@
-const TodoData =()=>{
+const TodoData =(props)=>{ //chỗn props này có thể truyền vô key /{name}/ của obj được truyền từ component cha (app.jsx) sang component con (tododata.jsx) cái này hơi vắng tắt nên suy sét khi sử dụng
+    //props ở đây mang kiểu đói tượng 
+
+    console.log('>>> check props', props);
+    const{ name ,age ,data}= props;
+    // const name =props.name ;
+    // const age = props.age ;
+    // const dâta = props.data ;
     return (
         <div className='todo-data'>
+            <div>my name is {name}</div>
+            {/* <div>Im {age} year old</div>
+            <div>where i am was born is {data.country}</div> */}
+            
             <div>Learning </div>
             <div>watching tv </div>
         </div>
     )
 }
-export {TodoData}
+export { TodoData };
