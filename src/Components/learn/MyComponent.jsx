992de8416a24db2c93e6b313e81a0284339f81bc
NAD00400 <anhduynguyenkhuat@gmail.com>
2024-09-26 10:56:42 +0700

    video31

--- src/Components/learn/MyComponent.jsx
+++ src/Components/learn/MyComponent.jsx
@@ -3,14 +3,27 @@ import './style.css';
 //bien namefunction =('tham so dau vao) =>{ lenh}
 const MyComponent =()=>{
     //html + js + css
+    // const hoidanit="eric";
+    // const hoidanit=25;
+    // const hoidanit=true;
+    // const hoidanit=undefined;
+    // const hoidanit=null;
+    const hoidanit=[1,2,3,4,5,6,7];
+    // const hoidanit={
+    //   name: 'nguyen anh duy',
+    //   age :23
+    // };
+
+
     return(
       <>
-        <div> Nguyen anh duy - hoidanit</div>
+        <div>{JSON.stringify(hoidanit)} - hoidanit update </div>
+        <div>{console.log('hello world from JSX on html')}</div>
         <div className="child"
               style={
                 {borderRadius: "10px;"}
               }
-        >tag </div>      
+        >child</div>      
       </>
     );
   };
