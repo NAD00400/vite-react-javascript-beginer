679b869c60384529afcd830db83077a9fcccef9e
NAD00400 <anhduynguyenkhuat@gmail.com>
2024-09-26 14:12:05 +0700

    video33

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,18 @@
-import { useState } from 'react'
-import './App.css'
 import reactLogo from './assets/react.svg'
-import MyComponent from './Components/learn/MyComponent'
-// import SecondComponent from './Components/learn/SecondComponent' import có default
-import { SecondComponent, ThirdComponent } from './Components/learn/SecondComponent'
-import viteLogo from '/vite.svg'
-
-
+import './Components/todo/todo.css'
+import { TodoData } from './Components/todo/TodoData'
+import { TodoNew } from './Components/todo/TodoNew'
 const App =()=> {
-  const [count, setCount] = useState(0)
-  // function myFunction (){
-  //   console.log('hello world from html');
-  // }
-  const myFunction =()=>{
-    console.log(' run my arrow function ');
-  };
-  myFunction();
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="todo-container">
+      <div className="todo-title">Todo List</div>
+      <TodoNew/>
+      <TodoData/>
+      <div className='todo-image'>
+        <img src={reactLogo} alt="React Logo" className='logo'/>
       </div>
-      <h1>Hello World</h1>
-      <MyComponent/>
-      <SecondComponent/>
-      <ThirdComponent/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+    
   )
 }
-
 export default App
---
+++ src/Components/todo/TodoData.jsx
@@ -0,0 +1,9 @@
+const TodoData =()=>{
+    return (
+        <div className='todo-data'>
+            <div>Learning </div>
+            <div>watching tv </div>
+        </div>
+    )
+}
+export {TodoData}
