9879bee47c8c721d35d108fd442fe3d8c93c9131
NAD00400 <anhduynguyenkhuat@gmail.com>
2024-09-27 14:10:00 +0700

    video36

--- src/Components/todo/TodoNew.jsx
+++ src/Components/todo/TodoNew.jsx
@@ -1,11 +1,23 @@
 const TodoNew=(props)=>{
     const {addNewToDo} =props ;
     // addNewToDo('duy')
+    const handleClick = ()=>{
+        alert('click me')
+    }
+    const handleOnChange=(name)=>{
+        console.log("handle on change",name );
+        
+    }
    return (   
         <div className='todo-new'>
-            <input type="text"/>
-            <button>Add</button>
+            <input type="text"
+                onChange ={(event)=>{handleOnChange(event.target.value)}}
+            />
+            <button
+                onClick={handleClick}
+            >Add</button>
         </div>
    )
 }
 export { TodoNew };
+ 
